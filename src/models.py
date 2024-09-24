889e460ff60e0865941cf5a814ab3d8d6901e6d1
Shafik Bahou <snb3000@gmail.com>
2024-09-24 14:53:22 +0000

    did the project with Julie

--- src/models.py
+++ src/models.py
@@ -1,32 +1,65 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    email = Column(String(250), nullable=False)
+    password = Column(String(256), nullable=False)
+    logins = relationship('Login', backref='user')
+    favorites = relationship('Favorite', backref='user')
 
     def to_dict(self):
-        return {}
+        return {"id": self.id, "name": self.name, "email": self.email}
+
+class Login(Base):
+    __tablename__ = 'login'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime, default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    success= Column(Boolean, default=False)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id', ondelete='CASCADE'), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id', ondelete='CASCADE'), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(250))
+    population = Column(Integer)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(Integer)
+    favorites = relationship('Favorite', backref='planet')
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(Integer)
+    mass = Column(Integer)
+    hair_color = Column(String(250))
+    favorites = relationship('Favorite', backref='character')
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
